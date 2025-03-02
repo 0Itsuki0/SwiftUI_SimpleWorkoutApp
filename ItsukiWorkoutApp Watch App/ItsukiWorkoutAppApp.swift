01ca31ee7ba770cce5e902f6799855511f5cfc83
itsuki <itsuki.enjyoy@gmail.com>
2025-03-02 09:17:09 +0900

    Initial Commit

---
+++ ItsukiWorkoutApp Watch App/ItsukiWorkoutAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ItsukiWorkoutAppApp.swift
+//  ItsukiWorkoutApp Watch App
+//
+//  Created by Itsuki on 2025/03/02.
+//
+
+import SwiftUI
+
+@main
+struct ItsukiWorkoutApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
